fae74c7b7efb5d7f3695437a76b4a0b5fd4e53d3
iambex <rbpascual1@upd.edu.ph>
2020-11-24 13:54:54 +0800

    Created functions makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,54 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     
+     #Setting the matrix
+     i<-NULL
+     set<-function(m){
+          x<-m
+          i<-NULL
+     }
+     
+     #Getting the matrix
+     get<-function(){
+          #Return matrix x
+          x
+     }
+     
+     #Setting the Inverse
+     setInverse-function(inverse){
+          i<-inverse
+     }
+     
+     #Getting the inverse
+     getInverse<-function(){
+          i
+     }
+     
+     #List of methods
+     list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     ## Return a matrix that is the inverse of 'x'
+}
+
+
+#Another function that computes the inverse from makeCacheMatrix
+cacheSolve<-function(x,...){
+     
+     i<-x$getInverse()
+     
+     if(!is.null(i)){
+          message("getting cached data")
+          return(i)
+     }
+     
+     newMatrix<-x$get()
+     inv<-solve(newMatrix)
+     x$setInverse(i)
+     i
 }
